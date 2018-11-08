2ee7d2de7f015309a19bdf93be24284a53ff79fb
Impact <support@gugy.eu>
2018-11-08 12:45:04 +0100

    Fix bad quoting of argument, add prints and check for existence of archives before downloading

--- build.sh
+++ build.sh
@@ -4,28 +4,44 @@ set -eu
 cd "$BUILD_DIR" || exit
 
 # OpenSSL
-wget https://www.openssl.org/source/openssl-1.1.1.tar.gz && tar -xzf openssl-1.1.1.tar.gz
+echo "Building openssl"
+if [[ ! -f "openssl-1.1.1.tar.gz" ]]; then
+	wget https://www.openssl.org/source/openssl-1.1.1.tar.gz && tar -xzf openssl-1.1.1.tar.gz
+fi
+
 cd openssl-1.1.1
 setarch i386 ./config -m32 no-shared && make
 mkdir lib && cp ./*.a lib/
 cd "$BUILD_DIR" || exit
 
 # Zlib
-wget http://zlib.net/zlib1211.zip && unzip -q zlib1211.zip
+echo "Building zlib"
+if [[ ! -f "zlib1211.zip" ]]; then
+	wget http://zlib.net/zlib1211.zip && unzip -q zlib1211.zip
+fi
+	
 cd zlib-1.2.11
 CFLAGS=-m32 ./configure -static && make
 mkdir include && mkdir lib && cp ./*.h include/ && cp libz.a lib
 cd "$BUILD_DIR" || exit
 
 # Libidn
-wget https://ftp.gnu.org/gnu/libidn/libidn2-2.0.5.tar.gz && tar -xzf libidn2-2.0.5.tar.gz
+echo "Building libidn"
+if [[ ! -f "libidn2-2.0.5.tar.gz" ]]; then
+	wget https://ftp.gnu.org/gnu/libidn/libidn2-2.0.5.tar.gz && tar -xzf libidn2-2.0.5.tar.gz
+fi
+
 cd libidn2-2.0.5
 CFLAGS=-m32 ./configure --disable-shared --enable-static --disable-doc && make
 mkdir include && cp lib/*.h include/ && cp lib/.libs/libidn2.a lib
 cd "$BUILD_DIR" || exit
 
 # LibCurl
-wget https://curl.haxx.se/download/curl-7.61.1.zip && unzip -q curl-7.61.1.zip
+echo "Building libcurl"
+if [[ ! -f "curl-7.61.1.zip" ]]; then
+	wget https://curl.haxx.se/download/curl-7.61.1.zip && unzip -q curl-7.61.1.zip
+fi
+
 cd curl-7.61.1
 ./configure --with-ssl="$BUILD_DIR/openssl-1.1.1" --with-zlib="$BUILD_DIR/zlib-1.2.11" \
  --with-libidn2="$BUILD_DIR/libidn2-2.0.5" --disable-shared --enable-static --disable-rtsp \
@@ -34,8 +50,12 @@ cd curl-7.61.1
 cd "$BUILD_DIR" || exit
 
 # SourceMod
-git clone https://github.com/alliedmodders/sourcemod --recursive --branch "$SMBRANCH" --single-branch "sourcemod-${SMBRANCH}"
+echo "Getting sourcemod"
+if [[ ! -d "sourcemod-${SMBRANCH}" ]]; then
+	git clone https://github.com/alliedmodders/sourcemod --recursive --branch "$SMBRANCH" --single-branch "sourcemod-${SMBRANCH}"
+fi
 
 
-cd "$MESSAGEBOT_DIR" ||exit
-make SMSDK="$BUILD_DIR/sourcemod-${SMBRANCH}" OPENSSL="$BUILD_DIR/openssl-1.1.1 ZLIB=$BUILD_DIR/zlib-1.2.11" IDN="$BUILD_DIR/libidn2-2.0.5" CURL="$BUILD_DIR/curl-7.61.1"
+echo "Building messagebot"
+cd "$MESSAGEBOT_DIR" || exit
+make SMSDK="$BUILD_DIR/sourcemod-${SMBRANCH}" OPENSSL="$BUILD_DIR/openssl-1.1.1" ZLIB="$BUILD_DIR/zlib-1.2.11" IDN="$BUILD_DIR/libidn2-2.0.5" CURL="$BUILD_DIR/curl-7.61.1"
